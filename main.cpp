8dc9c28e16cb8a3a83d32253ffe0f9a7d6146dac
Hiren Saravana <69997752+hirsar@users.noreply.github.com>
2020-08-21 11:25:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@ int main()
     Complex<double> var1 = 3.2_j+7.4;
     cout << "Complex variable declared: " << var1 << endl;
     Complex<double> var2 = 8.9_j+21.2;
-	cout << "Complex variable declared: " << var2 << endl;
-	Complex<int> var3 = 5_j+2;
-	cout << "Complex variable declared: " << var3 << endl;
-	Complex<int> var4 = -2_j+3;
-	cout << "Complex variabled declared: " << var4 << endl;
+    cout << "Complex variable declared: " << var2 << endl;
+    Complex<int> var3 = 5_j+2;
+    cout << "Complex variable declared: " << var3 << endl;
+    Complex<int> var4 = -2_j+3;
+    cout << "Complex variabled declared: " << var4 << endl;
     cout << "(" << var1 << ")" << " x " << "(" << var2 << ")" << " = " << var1*var2 << endl;
     cout << "(" << var1 << ")" << " / " << "(" << var2 << ")" << " = " << var1/var2 << endl;
-	cout << "(" << var3 << ")" << " x " << "(" << var4 << ")" << " = " << var3*var4 << endl;
-	cout << "(" << var3 << ")" << " / " << "(" << var4 << ")" << " = " << var3/var4 << endl;
+    cout << "(" << var3 << ")" << " x " << "(" << var4 << ")" << " = " << var3*var4 << endl;
+    cout << "(" << var3 << ")" << " / " << "(" << var4 << ")" << " = " << var3/var4 << endl;
 }
